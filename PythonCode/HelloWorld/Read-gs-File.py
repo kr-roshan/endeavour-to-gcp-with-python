e5727419ac57937f9a128f5b300da4231a80813a
Roshan Kumar <kr_roshan@hotmail.com>
2023-08-28 19:19:56 +0530

    few more hello world files

--- PythonCode/HelloWorld/Read-gs-File.py
+++ PythonCode/HelloWorld/Read-gs-File.py
@@ -1,1 +1,31 @@
+# pip install google-cloud-storage
+
+# python code to read file from public cloud storage bucket
+# https://storage.googleapis.com/pub/shakespeare/rose.txt (Public URL to view file)
+# gs://pub/shakespeare/rose.txt (gsUtil uri to view file)
+
 # input_file = 'gs://pub/shakespeare/rose.txt'
+
+# gs://public-rk/file1.json
+# https://storage.googleapis.com/public-rk/file1.json
+
+from google.cloud import storage
+
+# Initialize a client to interact with the Google Cloud Storage service
+client = storage.Client()
+
+# Specify the name of your Google Cloud Storage bucket and the path to the file
+bucket_name = 'public-rk'
+file_path = 'file1.json'
+
+# Get a reference to the bucket and file
+bucket = client.bucket(bucket_name)
+blob = bucket.blob(file_path)
+
+# Download the contents of the file
+file_contents = blob.download_as_text()
+
+# Print the contents of the file
+print(file_contents)
+
+
---
+++ PythonCode/HelloWorld/list-gce-instances.py
@@ -0,0 +1,40 @@
+# reference https://github.com/GoogleCloudPlatform/training-data-analyst/blob/master/courses/developingapps/python/devenv/list-gce-instances.py
+# pip install google-api-python-client
+
+# WHERE TO EXECUTE THIS?
+
+import argparse
+import os
+import time
+
+import googleapiclient.discovery
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items']
+
+
+def main(project, zone):
+    compute = googleapiclient.discovery.build('compute', 'v1')
+
+    instances = list_instances(compute, project, zone)
+
+    print('Instances in project %s and zone %s:' % (project, zone))
+    for instance in instances:
+        print(' - ' + instance['name'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('project_id', help='Your Google Cloud project ID.')
+    parser.add_argument(
+        '--zone',
+        default='us-central1-f',
+        help='Compute Engine zone to deploy to.')
+
+    args = parser.parse_args()
+
+    main(args.project_id, args.zone)
+
