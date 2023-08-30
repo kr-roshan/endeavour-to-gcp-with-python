62ace7b7f862240be18ebbbc2df01d428355f9f2
Roshan Kumar <kr_roshan@hotmail.com>
2023-08-31 02:43:07 +0530

    date time file

--- PythonCode/HelloWorld/ContextManager.py
+++ PythonCode/HelloWorld/ContextManager.py
@@ -1,6 +1,6 @@
 # https://www.geeksforgeeks.org/context-manager-in-python/
 # https://realpython.com/python-with-statement/
-
+# https://www.learndatasci.com/solutions/python-context-managers/
 
 
 
--- PythonCode/HelloWorld/DataTime.py
+++ PythonCode/HelloWorld/DataTime.py
@@ -1,9 +1,20 @@
+# try out various datetime functions in python
 
 import datetime
-import os
-yesterday = datetime.datetime.combine(
-    datetime.datetime.today() - datetime.timedelta(1),
-    datetime.datetime.min.time())
 
-print(datetime.datetime.now().strftime('%Y%m%d-%H%M%S'))
-print(os.sep)
+def PrintDateTime():
+    print(datetime.datetime.now())
+    print(datetime.datetime.today())
+    print(datetime.datetime.today() - datetime.timedelta(1))
+    today = datetime.datetime.combine(datetime.datetime.today(), datetime.datetime.min.time())
+    print(today)
+    yesterday = datetime.datetime.combine(datetime.datetime.today() - datetime.timedelta(1), datetime.datetime.min.time())
+    print(yesterday)
+    print(datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S'))
+    print(__name__)
+
+if __name__ == '__main__':
+    PrintDateTime()
+
+
+
