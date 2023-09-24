df8f66e6dd933e8740d340dfd8e48b49de310ab9
Roshan Kumar <kr_roshan@hotmail.com>
2023-09-25 02:14:39 +0530

    Composer BQ to GCS

---
+++ Composer/bq_copy_across_locations.py
@@ -0,0 +1,6 @@
+# Reference: https://github.com/GoogleCloudPlatform/python-docs-samples/blob/16de3b44f1a9faeccca5bc1cd8a719afc104dc65/composer/workflows/bq_copy_across_locations.py
+# Reference: Qwiklabs: Cloud Composer: Copying BigQuery Tables Across Different Locations: https://www.cloudskillsboost.google/focuses/3528?parent=catalog
+
+# https://cloud.google.com/blog/products/data-analytics/how-to-transfer-bigquery-tables-between-locations-with-cloud-composer
+# https://console.cloud.google.com/storage/browser/spls/gsp283/python-docs-samples
+
